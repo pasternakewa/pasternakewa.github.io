b6e17acf926324cb9b5649fdf4996adc63c24cb1
ewa <ewapasternak.kontakt@gmail.com>
2021-03-11 23:27:27 +0100

    Add basic stylingto expected content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import "./App.css";
+import "./App.scss";
+import photo from "./assets/photo.jpg";
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <div className="header">
+        <h2 className="header__name">Ewa Pasternak</h2>
+        <h2 className="header__title">Portfolio</h2>
+      </div>
+      <div className="intro">Intro</div>
+      <div className="portfolio">
+        <div className="portfolio__project--red">
+          <h4 className="portfolio__project__title">title</h4>
+          <img src={photo} alt="project-screenshot"></img>
+        </div>
+        <div className="portfolio__project--green">
+          <h4 className="portfolio__project__title">title</h4>
+          <img src={photo} alt="project-screenshot"></img>
+        </div>
+        <div className="portfolio__project--blue">
+          <h4 className="portfolio__project__title">title</h4>
+          <img src={photo} alt="project-screenshot"></img>
+        </div>
+      </div>
     </div>
   );
 }
