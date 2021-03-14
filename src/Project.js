85e791706f1150a20d7cbc2166f1297e12480290
ewa <ewapasternak.kontakt@gmail.com>
2021-03-14 19:50:35 +0100

    Add buttons leading to code and demo of project

--- src/Project.js
+++ src/Project.js
@@ -6,6 +6,14 @@ const Project = ({ backgroundColor, src, title }) => {
     <div className={projectClassName}>
       <h4 className="portfolio__project__title">{title}</h4>
       <img src={src} alt="project-screenshot"></img>
+      <div className="portfolio__project__buttons">
+        <a href="#" style={{ backgroundColor: "white" }}>
+          Code
+        </a>
+        <a href="#" style={{ backgroundColor: "#ececec" }}>
+          Demo
+        </a>
+      </div>
     </div>
   );
 };
