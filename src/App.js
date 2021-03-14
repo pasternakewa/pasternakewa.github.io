1a44d7b9c67c1dbdc0c91daa98916b26a702daa1
ewa <ewapasternak.kontakt@gmail.com>
2021-03-14 20:14:19 +0100

    Add basic footer structure

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Project from "./Project";
 import birthdayReminder from "./assets/birthday-reminder.png";
 import makeupShowroom from "./assets/makeup-showroom.png";
 import weatherApp from "./assets/weather-app.png";
+import Footer from "./Footer";
 
 const projects = [
   { src: makeupShowroom, title: "makeup-showroom", backgroundColor: "red" },
@@ -21,17 +22,20 @@ function App() {
         <h2 className="header__name">Ewa Pasternak</h2>
         <h2 className="header__title">Portfolio</h2>
       </div>
-      <div className="intro">Intro</div>
-      <div className="portfolio">
-        {projects.map(({ backgroundColor, src, title }) => (
-          <Project
-            backgroundColor={backgroundColor}
-            key={title}
-            src={src}
-            title={title}
-          />
-        ))}
+      <div className="content">
+        <div className="intro">Intro</div>
+        <div className="portfolio">
+          {projects.map(({ backgroundColor, src, title }) => (
+            <Project
+              backgroundColor={backgroundColor}
+              key={title}
+              src={src}
+              title={title}
+            />
+          ))}
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
