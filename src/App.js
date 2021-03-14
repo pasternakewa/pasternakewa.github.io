8acfe8024e3a3e814150308f11882f435387be91
ewa <ewapasternak.kontakt@gmail.com>
2021-03-14 21:45:30 +0100

    Add github and linkedin logos in intro instead of footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
 import Project from "./Project";
+import github from "./assets/github.jsx";
+import linkedin from "./assets/linkedin";
 import birthdayReminder from "./assets/birthday-reminder.png";
 import makeupShowroom from "./assets/makeup-showroom.png";
 import weatherApp from "./assets/weather-app.png";
-import Footer from "./Footer";
 
 const projects = [
   { src: makeupShowroom, title: "makeup-showroom", backgroundColor: "red" },
@@ -22,20 +23,29 @@ function App() {
         <h2 className="header__name">Ewa Pasternak</h2>
         <h2 className="header__title">Portfolio</h2>
       </div>
-      <div className="content">
-        <div className="intro">Intro</div>
-        <div className="portfolio">
-          {projects.map(({ backgroundColor, src, title }) => (
-            <Project
-              backgroundColor={backgroundColor}
-              key={title}
-              src={src}
-              title={title}
-            />
-          ))}
+      <div className="intro">
+        <div className="socials">
+          <a id="socials--github" href="https://github.com/pasternakewa">
+            {github}
+          </a>
+          <a
+            id="socials--inkedin"
+            href="https://www.linkedin.com/in/pasternakewa/"
+          >
+            {linkedin}
+          </a>
         </div>
       </div>
-      <Footer />
+      <div className="portfolio">
+        {projects.map(({ backgroundColor, src, title }) => (
+          <Project
+            backgroundColor={backgroundColor}
+            key={title}
+            src={src}
+            title={title}
+          />
+        ))}
+      </div>
     </div>
   );
 }
