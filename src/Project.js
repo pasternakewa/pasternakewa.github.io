88dd6f722cabad6e9217e1fe5627e687bc7d3369
ewa <ewapasternak.kontakt@gmail.com>
2021-03-14 22:00:45 +0100

    Add links to portfolio projects

--- src/Project.js
+++ src/Project.js
@@ -2,15 +2,28 @@ import React from "react";
 
 const Project = ({ backgroundColor, src, title }) => {
   const projectClassName = `portfolio__project--${backgroundColor}`;
+  const demoUrl = `https://pasternakewa.github.io/${title}`;
+  const codeUrl = `https://github.com/pasternakewa/${title}`;
+
   return (
     <div className={projectClassName}>
       <h4 className="portfolio__project__title">{title}</h4>
       <img src={src} alt="project-screenshot"></img>
       <div className="portfolio__project__buttons">
-        <a href="#" style={{ backgroundColor: "white" }}>
+        <a
+          href={codeUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ backgroundColor: "white" }}
+        >
           Code
         </a>
-        <a href="#" style={{ backgroundColor: "#ececec" }}>
+        <a
+          href={demoUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ backgroundColor: "#ececec" }}
+        >
           Demo
         </a>
       </div>
